3a03e2df41cc67b494cb404a0e8e5580fd515c46
chapseb <sebchap84@gmail.com>
2018-06-25 14:55:51 +0200

    Get name and description wordpress site

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,33 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // Display wordpress website name and description
+        $api = $this->getResource('/');
+        $wordpressName = $api->name;
+        $wordpressDescription = $api->description;
+
+        return $this->render('default/index.html.twig', [
+            'wordpress_name' => $wordpressName,
+            'wordpress_description' => $wordpressDescription,
+        ]);
+    }
+
+    /**
+     * Get data from resource wordpress api
+     *
+     * @param string $resource api url
+     *
+     * @return array/object
+     *
+     */
+    protected function getResource($resource)
+    {
+        $apiUrl = $this->container->getParameter('wordpress_url') . '/wp-json';
+        $json = file_get_contents($apiUrl.$resource);
+        $result = json_decode($json);
+        return $result;
+    }
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
